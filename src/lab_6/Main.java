56eca23c3e9db2ab924f6b7dcdbcaef74586905c
Nazar Leshchuk <lesnazarko18@gmail.com>
2018-07-04 19:36:47 +0300

    changes test file

--- src/lab_6/Main.java
+++ src/lab_6/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(final String[] args) {
 		try {
-			System.out.println(IJones.getInstance().ijones("src/lab_6/test3"));
+			System.out.println(IJones.getInstance().ijones("src/lab_6/test1"));
 		} catch (Exception e) {
 			System.out.println(e);
 		}
